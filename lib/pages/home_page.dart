9833bbd4aead674585c3e27111daf9bf462556b7
tangmengcheng <1018663766@qq.com>
2020-06-16 00:42:27 +0800

    add flutter_swiper

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_swiper/flutter_swiper.dart';
+const APPBAR_SCROLL_OFFSET = 100; // 最大滚动距离
 
 class HomePage extends StatefulWidget {
 
@@ -9,12 +11,76 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
 
+  List _imageUrls = [
+    'http://pages.ctrip.com/commerce/promote/20180718/yxzy/img/640sygd.jpg',
+    'https://dimg04.c-ctrip.com/images/700u0r000000gxvb93E54_810_235_85.jpg',
+    'https://dimg04.c-ctrip.com/images/700c10000000pdili7D8B_780_235_57.jpg'
+  ];
+
+  double appBarAlpha = 0;
+
+  _onScroll(offset) {
+    double alpha = offset / APPBAR_SCROLL_OFFSET;
+    if(alpha < 0) {
+      alpha = 0;
+    } else if(alpha > 1) {
+      alpha = 1;
+    }
+    setState(() {
+      appBarAlpha = alpha;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text('首页'),
-      ),
+      body: Stack(
+        children: [
+          MediaQuery.removePadding(removeTop: true, context: context,
+              child: NotificationListener(
+                onNotification: (scrollNotification) {
+                  if(scrollNotification is ScrollUpdateNotification && scrollNotification.depth == 0) {
+                    // 滚动且是列表滚动的时候
+                    _onScroll(scrollNotification.metrics.pixels);
+                  }
+                },
+                child: ListView(
+                  children: [
+                    Container(
+                      height: 160,
+                      child: Swiper(
+                        itemCount: _imageUrls.length,
+                        autoplay: true,
+                        itemBuilder: (BuildContext context, int index) {
+                          return Image.network(
+                            _imageUrls[index],
+                            fit: BoxFit.fill,
+                          );
+                        },
+                        pagination: SwiperPagination(),
+                      ),
+                    ),
+                    Container(
+                      height: 800,
+                      child: ListTile(title: Text('你好'),),
+                    )
+                  ],
+                ),
+              )
+          ),
+          Opacity(
+            opacity: appBarAlpha,
+            child: Container(
+              height: 80,
+              decoration: BoxDecoration(color: Colors.white),
+              child: Center(
+                child: Padding(padding: EdgeInsets.only(top: 20),
+                child: Text('首页'),),
+              ),
+            ),
+          )
+        ],
+      )
     );
   }
 }
